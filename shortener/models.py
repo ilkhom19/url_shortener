55151abfdcddc65eba6ece45294a6276f7fcb08b
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-08-08 12:17:10 +0600

    added pagination and filters

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,11 @@
 from hashlib import md5
 
 from django.db import models
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+from graphql import GraphQLError
+
 
 class URL(models.Model):
     full_url = models.CharField(unique=True, max_length=2024)
@@ -15,5 +20,12 @@ class URL(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.url_hash = md5(self.full_url.encode()).hexdigest()[:8]
+
+        validate = URLValidator()
+        try:
+            validate(self.full_url)
+        except ValidationError as e:
+            raise GraphQLError('invalid url')
+    
 
         return super().save(*args, **kwargs)
