16f6b9d8446082afa511b99bdcdbfa492f26ee8b
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-08-08 12:08:30 +0600

    confihured GraphQL

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,19 @@
+from hashlib import md5
+
 from django.db import models
 
-# Create your models here.
+class URL(models.Model):
+    full_url = models.CharField(unique=True, max_length=2024)
+    url_hash = models.CharField(unique=True, max_length=8)
+    clicks = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clicked(self):
+        self.clicks += 1
+        self.save()
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:8]
+
+        return super().save(*args, **kwargs)
